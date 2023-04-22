ebcd882ca08b84bfe0d1650e4a37002bb45146ae
Valkyrie-freja <yadokame3.14@gmail.com>
2023-04-22 19:58:13 +0900

    move method change to editing velocity

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -16,6 +16,23 @@ public class Walk : MonoBehaviour
     void Update()
     {
         Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        const float x_max = 25.0f;
+        const float z_max = 25.0f;
+        if (transform.position.x > x_max)
+        {
+            Transform current_transform = this.transform;
+            Vector3 new_position = current_transform.position;
+            new_position.x = 0;
+            current_transform.position = new_position;
+        }
+
+        if (transform.position.z > z_max)
+        {
+            Transform current_transform = this.transform;
+            Vector3 new_position = current_transform.position;
+            new_position.z = 0;
+            current_transform.position = new_position;
+        }
     }
 
     void Move(float x_input, float z_input)
@@ -28,24 +45,16 @@ public class Walk : MonoBehaviour
         }
         
         // move
-        const float speed = 5.0f;
-        float move_length = Mathf.Sqrt(x_input * x_input + z_input * z_input);
-        float length_inv = 1f / move_length;
         animator.SetBool("isWalking", true);
-
-        Vector3 forceInput = new Vector3(x_input, 0, z_input);
-        // 粘性抵抗
-        Vector3 resistViscosity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z);
-        // 慣性抵抗
-        Vector3 resistInertia = new Vector3(
-            rigidbody.velocity.x * rigidbody.velocity.x,
-            rigidbody.velocity.y * rigidbody.velocity.y,
-            rigidbody.velocity.z * rigidbody.velocity.z);
-        Vector3 forceEffective = forceInput - resistViscosity - resistInertia;
-        rigidbody.AddForce(forceEffective);
+        Vector3 velocityInput = new Vector3(x_input, 0, z_input);
+        float move_length_inv = 1f / velocityInput.magnitude;
+        Vector3 velocityInput_norm = velocityInput * move_length_inv;
+        const float speed = 5.0f;
+        Vector3 velocityEffective = velocityInput_norm * speed;
+        rigidbody.velocity = velocityEffective;
         
         // rotate
-        Vector3 targetDirection = new Vector3(x_input, 0, z_input);
+        Vector3 targetDirection = velocityInput;
         if(targetDirection.magnitude > 0.1)
         {
             const float smooth = 10.0f;
