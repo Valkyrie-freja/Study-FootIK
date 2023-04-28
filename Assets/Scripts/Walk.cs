680fcac47f84d6336efc3f01417115d124b1807d
Valkyrie-freja <yadokame3.14@gmail.com>
2023-04-29 02:32:24 +0900

    test ToCameraStandardVector

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -17,6 +17,17 @@ public class Walk : MonoBehaviour
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
         mainCamera = Camera.main.gameObject;
+        
+        // test
+        for(float x = -1.0f; x <= 1.0f ; x += 0.1f)
+        {
+            for(float z = -1.0f; z <= 1.0f; z += 0.1f)
+            {
+                Vector3 input_vector3 = new Vector3(x, 0, z);
+                Vector3 camera_standard_vector = ToCameraStandardVector(input_vector3);
+                Debug.Log($"{input_vector3}, {camera_standard_vector}");
+            }
+        }
     }
 
     // Update is called once per frame
@@ -44,10 +55,11 @@ public class Walk : MonoBehaviour
 
     private Vector3 ToCameraStandardVector(Vector3 _input_vector)
     {
-        Quaternion input_rotate = Quaternion.FromToRotation(Vector3.forward, _input_vector);
+        Quaternion input_rotate = Quaternion.FromToRotation(Vector3.forward/*(0, 0, 1)*/, _input_vector);
         Vector3 camera_look_vector = mainCamera.transform.forward;
-        
-        return input_rotate * camera_look_vector;
+        // use only x and z
+        camera_look_vector.y = 0.0f;
+        return input_rotate * camera_look_vector.normalized;
     }
     
     void Move(float x_input, float z_input)
@@ -62,7 +74,7 @@ public class Walk : MonoBehaviour
         // move
         animator.SetBool("isWalking", true);
         Vector3 input_vector = new Vector3(x_input, 0, z_input);
-        Vector3 player_look_vector = ToCameraStandardVector(input_vector); // normalized maybe
+        Vector3 player_look_vector = ToCameraStandardVector(input_vector);
         
         const float speed = 5.0f;
         Vector3 velocityEffective = player_look_vector * speed;
