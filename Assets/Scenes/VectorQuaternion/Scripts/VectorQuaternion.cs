fcb978fdc4f0d266f4a2963d945582f4206cda3e
Valkyrie-freja <yadokame3.14@gmail.com>
2023-04-29 21:13:33 +0900

    make to quaternion func.

--- Assets/Scenes/VectorQuaternion/Scripts/VectorQuaternion.cs
+++ Assets/Scenes/VectorQuaternion/Scripts/VectorQuaternion.cs
@@ -24,6 +24,7 @@ public class VectorQuaternion : MonoBehaviour
             Input.GetAxis("Vertical"));
         Vector3 player_look_vector = 
             ToCameraStandardVector(controller_input_vector);
+        SetArrowStatus(arrow_player_look, player_look_vector);
     }
 
     private void SetArrowStatus(GameObject _arrow, float _scale, Quaternion _rotate)
@@ -32,28 +33,35 @@ public class VectorQuaternion : MonoBehaviour
         _arrow.transform.rotation = _rotate;
     }
     
-    private void SetArrowStatus(GameObject _arrow, float _scale, Vector3 _vector3)
+    private void SetArrowStatus(GameObject _arrow, Vector3 _vector3)
     {
-        _arrow.transform.localScale = _scale * Vector3.one;
-        Quaternion _rotate = Quaternion.Euler(_vector3);
-        _arrow.transform.rotation = _rotate;
+        _arrow.transform.localScale = _vector3.magnitude * Vector3.one;
+        Quaternion rotate = ToQuaternion(_vector3);
+        _arrow.transform.rotation = rotate;
+    }
+
+    private Quaternion ToQuaternion(Vector3 _vector)
+    {
+        Quaternion rotate = 
+            Quaternion.FromToRotation(Vector3.forward, _vector);
+        // when (0, 0, 0) -> (0, 0, -1), not to be rotate = 0
+        if (_vector.x == 0 && _vector.z < 0) {
+            rotate = Quaternion.AngleAxis(180, Vector3.up);
+        }
+
+        return rotate;
     }
     
     private Vector3 ToCameraStandardVector(Vector3 _input_vector)
     {
-        Quaternion input_rotate = 
-            Quaternion.FromToRotation(Vector3.forward, _input_vector);
-        // when (0, 0, 0) -> (0, 0, -1), not to be rotate = 0
-        if (_input_vector.x == 0 && _input_vector.z < 0) {
-            input_rotate = Quaternion.AngleAxis(180, Vector3.up);
-        }
+        Quaternion input_rotate = ToQuaternion(_input_vector);
         SetArrowStatus(arrow_input, _input_vector.magnitude, input_rotate);
 
-        Vector3 camera_look_vector = camera.transform.forward;
+        Vector3 camera_look_vector = - camera.transform.forward;
         // use only x and z
         camera_look_vector.y = 0.0f;
-        SetArrowStatus(arrow_camera, 1.0f, camera_look_vector);
+        SetArrowStatus(arrow_camera, camera_look_vector);
         
-        return input_rotate * camera_look_vector.normalized;
+        return _input_vector.magnitude * (input_rotate * camera_look_vector);
     }
 }
